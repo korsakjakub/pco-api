1ba0103792f7f318a166f78ca01bb1eef6317fac
Jakub Korsak <jakub@korsak.xyz>
2023-07-20 12:39:24 +0200

    What can go wrong?

--- src/main/java/xyz/korsak/pcoapi/BaseController.java
+++ src/main/java/xyz/korsak/pcoapi/BaseController.java
@@ -1,5 +1,8 @@
 package xyz.korsak.pcoapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
 public class BaseController {
     protected String extractBearerToken(String authorizationHeader) {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
