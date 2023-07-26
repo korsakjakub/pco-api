93d7358edd06de99dc2514259d30a77fecbf20f4
Jakub Korsak <jakub@korsak.xyz>
2023-07-26 15:55:53 +0200

    Remove unnecessary annotations

---
+++ src/main/java/xyz/korsak/pcoapi/responses/IdResponse.java
@@ -0,0 +1,12 @@
+package xyz.korsak.pcoapi.responses;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class IdResponse {
+    private String id;
+}
