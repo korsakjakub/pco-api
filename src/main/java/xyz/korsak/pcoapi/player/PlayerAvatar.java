2b521c776885d150024cd90ee64bd6d5c382d60f
Jakub Korsak <jakub.korsak@protonmail.com>
2024-05-20 21:25:09 +0200

    fix: probability is a number

--- src/main/java/xyz/korsak/pcoapi/player/PlayerAvatar.java
+++ src/main/java/xyz/korsak/pcoapi/player/PlayerAvatar.java
@@ -4,5 +4,5 @@ public record PlayerAvatar(String accessories, String accessoriesColor, int acce
                            String backgroundColor, String clothesColor, String clothing, String clothingGraphic,
                            String eyebrows, String eyes, String facialHair, String facialHairColor,
                            int facialHairProbability, String hairColor, String hatColor, String mouth, String nose,
-                           String skinColor, String top, String topProbability) {
+                           String skinColor, String top, int topProbability) {
 }
