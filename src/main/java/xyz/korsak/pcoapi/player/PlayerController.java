5586e84b4bdd21d1ab42d331c41de8a095d85b94
Jakub Korsak <jakub@korsak.xyz>
2024-11-04 10:38:33 +0100

    feat: implement toggleSittingOut controller (#33)
    * feat: implement toggleSittingOut controller
    
    * omit player state transition during round start
    
    * fix: toggle switch request had improper structure

--- src/main/java/xyz/korsak/pcoapi/player/PlayerController.java
+++ src/main/java/xyz/korsak/pcoapi/player/PlayerController.java
@@ -62,4 +62,15 @@ public class PlayerController extends BaseController {
         return logResponse(ResponseEntity.ok(new IdResponse(playerId)));
     }
 
+    @PostMapping("/{playerId}/sit-out")
+    public ResponseEntity<Boolean> toggleSittingOut(@RequestParam String roomId,
+                                                    @PathVariable String playerId,
+                                                    @RequestHeader("Authorization") String authorizationHeader) {
+        String playerToken = extractBearerToken(authorizationHeader);
+        roomService.toggleSittingOut(roomId, playerId, playerToken);
+
+        roomService.pushData(roomId);
+        return logResponse(ResponseEntity.ok(true));
+    }
+
 }
